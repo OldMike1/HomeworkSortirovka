5640d83912c3c38f19cd64e2c565ac61826453e5
mike19952006 <mike19952006@gmail.com>
2018-03-09 22:29:53 +0300

    Сортировка массива по возрастанию.

---
+++ HomeworkSortirovka/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkSortirovka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 4, 7, 8, 3, 2, 4, 6, 12, 100 };
+            int a;
+            int b;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        a = array[i];
+                        b = array[j];
+                        array[i] = b;
+                        array[j] = a;
+                    }
+                }
+            }
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
